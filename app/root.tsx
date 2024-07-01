4cb48ee2c77de9b010502f2fb699a62c25a91b5a
Brian Smiley <briantsmiley42@gmail.com>
2024-07-01 11:17:57 -0400

    setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,33 @@
+import type { MetaFunction, LoaderFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
-import "./tailwind.css";
+
+// Import rootAuthLoader
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+
+//import ClerkApp
+import { ClerkApp } from "@clerk/remix";
+
+export const meta: MetaFunction = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1"
+  }
+];
+
+// Export as the root route loader
+export const loader: LoaderFunction = args => rootAuthLoader(args);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
@@ -25,6 +40,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+function App() {
   return <Outlet />;
 }
+
+export default ClerkApp(App);
